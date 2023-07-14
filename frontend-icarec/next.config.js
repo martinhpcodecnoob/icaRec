6fd2fba719737ac82aa60b6c398a7b7f02bbe4ca
martinhpcodecnoob <hp.u.smile@gmail.com>
2023-07-13 20:17:50 -0500

    Se va a subir el home falta hacerlo mas responsive

--- frontend-icarec/next.config.js
+++ frontend-icarec/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-}
+images: {
+    remotePatterns: [
+    {
+        hostname: "rickandmortyapi.com",
+    },
+    ],
+},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- frontend-icarec/src/app/prueba2/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function pruebaindex() {
-  return (
-    <div>pruebaindex</div>
-  )
-}
