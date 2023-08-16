86ad833464d4b5123dd42b9ff8b9f4621ffca563
JulioXC4 <danielc4nt@gmail.com>
2023-08-16 01:23:57 -0500

    aggregate component session provider

--- frontend-icarec/src/app/layout.js
+++ frontend-icarec/src/app/layout.js
@@ -1,52 +1,58 @@
-'use client'
-import { SessionProvider } from "next-auth/react";
+
 import './globals.css'
 import { Raleway } from 'next/font/google'
 import Script from "next/script";
 import ProvidersRedux from "@/redux/provider";
-
+import { NextAuthProvider } from "@/components/SesionProvider";
+ 
 const raleway = Raleway({ subsets: ['latin'] })
 
 const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID
 
+export const metadata = {
+  title: 'Tiendas é',
+  description: 'Somos tiendas é',
+}
+ 
 export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-        <head>
-          <Script
-            strategy='beforeInteractive'
-            src={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=Function.prototype`}
-            // Este script tiene un API_KEY pero el ultimo es una funcion pero puede ser cualquier funciona que pongas
-          />
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-            id="google-analytics1"
-          />
-          <Script id="google-analytics2">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+      <head>
+        <Script
+          strategy='beforeInteractive'
+          src={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=Function.prototype`}
+          // Este script tiene un API_KEY pero el ultimo es una funcion pero puede ser cualquier funciona que pongas
+        />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          id="google-analytics1"
+        />
+        <Script id="google-analytics2">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-            gtag('config', '${GOOGLE_ANALYTICS_ID}');
-            `}
-          </Script>
-          <Script
-            id="Adsense-id"
-            data-ad-client={GOOGLE_ADS_ID}
-            async="true"
-            strategy="beforeInteractive"
-             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          />
-        </head>
+          gtag('config', '${GOOGLE_ANALYTICS_ID}');
+          `}
+        </Script>
+        <Script
+          id="Adsense-id"
+          data-ad-client={GOOGLE_ADS_ID}
+          async="true"
+          strategy="beforeInteractive"
+           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+      </head>
+ 
           <ProvidersRedux>
             <body className={raleway.className}>
-              <SessionProvider>
+              <NextAuthProvider>
                 {children}
-              </SessionProvider>
+              </NextAuthProvider>
             </body>
           </ProvidersRedux>
     </html>
