4fa5387297f6037d5c4858722de27033a8d86332
martinhpcodecnoob <hp.u.smile@gmail.com>
2023-08-23 11:57:39 -0500

    Probando las metadatas

--- frontend-icarec/src/app/layout.js
+++ frontend-icarec/src/app/layout.js
@@ -12,8 +12,27 @@ const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID
 
 export const metadata = {
-  title: 'Tiendas é',
-  description: 'Somos tiendas é',
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
  
 export default function RootLayout({ children }) {
