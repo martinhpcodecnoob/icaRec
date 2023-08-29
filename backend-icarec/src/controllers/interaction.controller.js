af7aa8d39008e74619ff93f232342c0128292c52
JulioXC4 <danielc4nt@gmail.com>
2023-08-28 19:17:38 -0500

    Controller to update interactions modified

--- backend-icarec/src/controllers/interaction.controller.js
+++ backend-icarec/src/controllers/interaction.controller.js
@@ -22,8 +22,16 @@ const create_interaction = async(req,res) =>{
 
 const update_interaction = async (req, res) => {
     try {
-        const { interactionId, uppdatedFields } = req.body
+        const { interactionId, liked } = req.body
+
+      if (!interactionId) {
+        return res.status(400).json({ updated: false, message: 'Interaction ID is required.' })
+      }
   
+      if(liked === undefined || liked === null || (liked !== true && liked !== false)){
+        return res.status(400).json({ updated: false, message: 'The value of the "liked" parameter must be true or false.' })
+      } 
+
       const existingInteraction = await Interaction.findById(interactionId)
       if (!existingInteraction) {
         return res.status(404).json({ message: "Interaction not found" })
@@ -31,11 +39,11 @@ const update_interaction = async (req, res) => {
   
       const updatedInteraction = await Interaction.findByIdAndUpdate(
         interactionId,
-        updatedFields,
+        { liked },
         { new: true } // Devuelve la interacción actualizada en la respuesta en vez de usar el .save()
       )
   
-      return res.status(200).json(updatedInteraction)
+      return res.status(200).json({ updated: true, message: "Interaction updated successfully.", updatedInteraction })
     } catch (error) {
       return res.status(500).json({
         updated: false,
