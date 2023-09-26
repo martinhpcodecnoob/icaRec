c8a6bc8ccd82bd43229b4018c78879757848ba2c
martinhpcodecnoob <hp.u.smile@gmail.com>
2023-09-25 20:05:09 -0500

    Cambios 3

--- frontend-icarec/src/app/page.jsx
+++ frontend-icarec/src/app/page.jsx
@@ -2,7 +2,7 @@ import LandingPage from './LandingPage'
 
 export async function getAllBusiness(){
   try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URI}/api/business/getAllBusiness`,{next:{revalidate:2}})
+      const response = await fetch(`http://localhost:3004/api/business/getAllBusiness`,{next:{revalidate:2}})
       const business = response.json()
       return business
   } catch (error) {
