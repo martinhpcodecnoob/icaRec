6c1a375aa27235fca55b79a4df7c163f370c1bdf
JulioXC4 <danielc4nt@gmail.com>
2024-06-03 23:53:59 -0500

    Fixed the error when fetching the countries API

--- frontend-icarec/src/components/Register/PhoneSection.jsx
+++ frontend-icarec/src/components/Register/PhoneSection.jsx
@@ -1,123 +1,134 @@
-'use client'
+"use client";
 
-import React, {useState, useEffect} from 'react'
-import { Controller } from 'react-hook-form'
-import { fetchCountries } from '../../../utils/apiServices'
+import React, { useState, useEffect } from "react";
+import { Controller } from "react-hook-form";
+import { fetchCountries } from "../../../utils/apiServices";
 
-const PhoneSection = ({control, errors, setValue, getValues}) => {
-
-  const [showPhoneInput, setshowPhoneInput] = useState(false)
-  const [countries, setCountries] = useState([])
-  const [countryCode, setCountryCode] = useState('')
-  const [selectedCountry, setSelectedCountry] = useState('')
+const PhoneSection = ({ control, errors, setValue, getValues }) => {
+  const [showPhoneInput, setshowPhoneInput] = useState(false);
+  const [countries, setCountries] = useState([]);
+  const [countryCode, setCountryCode] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const countriesOptions = (
     <>
-     <option value="default">Selecciona un país</option>
-    {countries.map((country, index) => (
-      <option key={`country-${country.code}-${index}`} value={country.code}>
-        {country.name}
-      </option>
-    ))}
+      <option value="default">Selecciona un país</option>
+      {countries.map((country, index) => (
+        <option key={`country-${country.code}-${index}`} value={country.code}>
+          {country.name}
+        </option>
+      ))}
     </>
-  )
+  );
 
   useEffect(() => {
     fetchCountries()
-      .then((data) => setCountries(data))
-      .catch((error) => console.error("Error fetching countries:", error))
-  }, [])
+      .then((data) => {
+        setCountries(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching countries:", error);
+        setCountries([{ code: "PE", name: "Perú", callingCodes: ["51"] }]);
+        setLoading(false);
+      });
+  }, []);
 
   const handleCountryChange = (e) => {
-    const selectedCode = e.target.value
-    const countryName = selectedCode
-    const selectedCountry = countries.find((country) => country.name === countryName)
+    const selectedCode = e.target.value;
+    const selectedCountry = countries.find(
+      (country) => country.code === selectedCode
+    );
     if (selectedCountry) {
-      setSelectedCountry(selectedCode)
-      // Usamos la función de devolución de llamada para asegurarnos de que countryCode se actualice inmediatamente
+      setSelectedCountry(selectedCode);
       setCountryCode((prevCountryCode) => {
-        const newCountryCode = `+${selectedCountry.callingCodes[0]}`
-        setValue('phoneCode', newCountryCode)
-        return newCountryCode
-      })
-  
-      setshowPhoneInput(true)
-    } 
-  }
+        const newCountryCode = `+${selectedCountry.callingCodes[0]}`;
+        setValue("phoneCode", newCountryCode);
+        return newCountryCode;
+      });
+
+      setshowPhoneInput(true);
+    }
+  };
 
   return (
-    <div className='w-full mb-2'>
-      <div className='grid grid-cols-10'>
-      { showPhoneInput ? (
-        <>
-          <div className='col-span-2 px-3 py-1'>
-            <span className='text-[#100e80] font-semibold'>             
-              Celular
-            </span>
-          </div>
-          <div className='col-span-2 col-start-3'>
-            <button 
-            className={`border rounded-full px-3 py-1 text-[#100e80] font-semibold bg-[#f3ba1a] ${
-              showPhoneInput ? '' : 'hidden'
-            }`}
-            onClick={() => {
-              setshowPhoneInput(false)
-               setValue('phoneCode', '')
-              setValue('phoneNumber', '')
-              }}
-            >
-              {countryCode}
-            </button>
-          </div>
-          <div div className='col-span-6'>
-          <Controller
+    <div className="w-full mb-2">
+      <div className="grid grid-cols-10">
+        {showPhoneInput ? (
+          <>
+            <div className="col-span-2 px-3 py-1">
+              <span className="text-[#100e80] font-semibold">Celular</span>
+            </div>
+            <div className="col-span-2 col-start-3">
+              <button
+                className={`border rounded-full px-3 py-1 text-[#100e80] font-semibold bg-[#f3ba1a] ${
+                  showPhoneInput ? "" : "hidden"
+                }`}
+                onClick={() => {
+                  setshowPhoneInput(false);
+                  setValue("phoneCode", "");
+                  setValue("phoneNumber", "");
+                }}
+              >
+                {countryCode}
+              </button>
+            </div>
+            <div className="col-span-6">
+              <Controller
                 name="phoneNumber"
                 control={control}
                 defaultValue=""
                 render={({ field }) => (
                   <input
                     {...field}
-                    name='phoneNumber'
-                    className='border rounded-full py-1 px-3 placeholder-white text-white bg-[#f3ba1a] w-full'
+                    name="phoneNumber"
+                    className="border rounded-full py-1 px-3 placeholder-white text-white bg-[#f3ba1a] w-full"
                     placeholder="Número de celular"
                   />
                 )}
               />
-          </div>
-          {errors.phoneNumber && showPhoneInput && (
-            <p className='col-span-10 text-red-500'>{errors.phoneNumber.message}</p>
-          )}
-        </>
-      ) : (
-        <>
-          <div className='col-span-10'>
-            <Controller 
-              name='phoneNumber'
-              control={control}
-              defaultValue=""
-              render={({field}) => (
-                <select 
-                  {...field}
-                  name='phoneNumber'
-                  className='border rounded-full py-1 px-3 bg-[#f3ba1a] text-[#100e80] font-semibold w-full'
-                  onChange={(e) => {
-                    handleCountryChange(e)
-                  }}
-                >
-                  {countriesOptions}
-                </select>
-              )}
+            </div>
+            {errors.phoneNumber && showPhoneInput && (
+              <p className="col-span-10 text-red-500">
+                {errors.phoneNumber.message}
+              </p>
+            )}
+          </>
+        ) : (
+          <>
+            <div className="col-span-10">
+              <Controller
+                name="phoneNumber"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <select
+                    {...field}
+                    name="phoneNumber"
+                    className="border rounded-full py-1 px-3 bg-[#f3ba1a] text-[#100e80] font-semibold w-full"
+                    onChange={(e) => {
+                      handleCountryChange(e);
+                    }}
+                    disabled={loading}
+                  >
+                    {loading ? (
+                      <option value="loading">Cargando...</option>
+                    ) : (
+                      countriesOptions
+                    )}
+                  </select>
+                )}
               />
-          </div>
-          {errors.selectedCountry && !showPhoneInput && (
-            <p className='col-span-10 text-red-500'>Selecciona un país</p>
-          )}
-        </>
-        ) 
-      }
+            </div>
+            {errors.selectedCountry && !showPhoneInput && (
+              <p className="col-span-10 text-red-500">Selecciona un país</p>
+            )}
+          </>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PhoneSection
+export default PhoneSection;
